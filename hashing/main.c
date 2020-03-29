e92b7a08a83ee15a33a40c1e423a3ffe1014da58
ahtishamuddin <ahtishamuddin44@gmail.com>
2020-03-30 04:29:19 +0500

    linear probing using floating point and using strings

--- hashing/main.c
+++ hashing/main.c
@@ -1,81 +1,156 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>
+#include <stdbool.h>
 
-#define size 7
+#define max_name 256
+#define tablesize 10
 
-struct node
+typedef struct
+    {
+
+        char name [max_name];
+    }person;
+//making a hash table its an array of pointers to people struct , pointers because for pointers i don't need space for tables means i directly fill the table
+// 2nd reason to use the pointers that it makes easy for me to tell when a spot in the table is empty because i can just set the pointer to null
+//making a function to setup the table
+person * hash_table[tablesize];
+void init_hashtable()
 {
-    int data;
-    struct node *next;
-};
-
-struct node *chain[size];
-
-void init()
-{
-    int i;
-    for(i = 0; i < size; i++)
-        chain[i] = NULL;
+    //initially i want all the entries to be empty so :
+    for(int i = 0; i <tablesize ; i ++)
+    {
+        hash_table [i] = NULL;
+    }
+    //table is empty
 }
 
-void insert(int value)
+// i am declaring hash func which takes a input name and map it to a location in a table  which is an unsigned int:
+unsigned int hash (char *name )
 {
-    //create a newnode with value
-    struct node *newNode = malloc(sizeof(struct node));
-    newNode->data = value;
-    newNode->next = NULL;
+    int length = strlen(name ); //it woudl calculate the length of the string
+    //now i would go through the string at one character at a time and add the  ASCII values of char
+    unsigned int hash_value = 0;
+    for(int i =0 ;i <length ; i++)
+    {
+        hash_value += name [i]; //we would add each char ascii value to the sum
+        hash_value =  ( hash_value * name[i] ) ; //for getting more random number
+        hash_value =  hash_value % tablesize ; //keeping the num bewtween 0 and table size
 
-    //calculate hash key
-    int key = value % size;
 
-    //check if chain[key] is empty
-    if(chain[key] == NULL)
-        chain[key] = newNode;
-    //collision
+    }
+    return hash_value ; //we can return the sum when we are done
+}
+//making a function that would insert something in the table
+//this func would return a boolean true if we successfully inserted otherwise false
+bool hash_table_insert(person *p)
+{
+    if( p == NULL) //if it is null value
+    {
+        return false ;
+    }
+    int index = hash(p->name); //index or location where we are going to try the index of a person .
+    //making linear probing :
+    for(int i =0 ; i <tablesize ; i++){
+        int key = (i+index) % tablesize ;
+        if(hash_table[key] == NULL)
+        {
+            hash_table[key] = p;
+            return true;
+        }
+    } return false ;
+
+}
+
+void print_table ()
+{
+    printf("Start \n");
+    for(int i = 0 ; i<tablesize ; i++)
+    {
+        if(hash_table[i] == NULL)
+        {
+            printf(" %i \t-----\n" ,i);
+        }
+        else
+        {
+            printf(" %i\t%s\n",i,hash_table[i]->name);
+
+        }
+    }
+    printf("End \n \n");
+}
+//the main concept of the table is too lookup people
+//so now lets make a lookup function
+//it find a person on the table by their name
+person *hash_table_lookup (char *name)
+{
+    int index = hash(name);
+    //adding linear logic in the lookup
+    for(int i = 0 ; i<tablesize ; i++)
+    {
+        int key = (index +i) %tablesize;
+        if(hash_table[key] != NULL && strncmp(hash_table[key]->name , name , 50)== 0)
+    { //strncmp() func is used to compare two strings with each other to limited no of char passed
+        return hash_table[key];
+    }
+    }
+    return NULL ;
+
+}
+
+int main(){
+    init_hashtable();
+    print_table();
+    //lets make some people
+    person jacob = {.name = "jacob"};
+    person kate = {.name = "kate"};
+    person mpho = {.name = "mpho"};
+    person Sarah = {.name = "Sarah"};
+    person Edna = {.name = "Edna"};
+    person Eliza = {.name = "Eliza"};
+    person Robert = {.name = "Robert"};
+    person Jane = {.name = "Jane"};
+
+
+    //inserting these people in the hashtable
+    hash_table_insert(&jacob);
+    hash_table_insert(&kate);
+    hash_table_insert(&mpho);
+    hash_table_insert(&Sarah);
+    hash_table_insert(&Edna);
+    hash_table_insert(&Eliza);
+    hash_table_insert(&Robert);
+    hash_table_insert(&Jane);
+
+    //print
+    print_table();
+
+    //looking up  a name in the table :
+    person *tmp = hash_table_lookup("mpho");
+
+
+    if(tmp == NULL )
+    {
+        printf("we don't found it \n ");
+    }
     else
     {
-        //add the node at the end of chain[key].
-        struct node *temp = chain[key];
-        while(temp->next != NULL)
-        {
-            temp = temp->next;
-        }
-
-        temp->next = newNode;
+        printf("Found %s \n",tmp ->name);
     }
-}
-
-void print()
-{
-    int i;
-
-    for(i = 0; i < size; i++)
+    //lookin up a value which is not present in the table
+    tmp = hash_table_lookup("george");
+     if(tmp == NULL )
     {
-        struct node *temp = chain[i];
-        printf("chain[%d]---",i);
-        while(temp)
-        {
-            printf("%d---",temp->data);
-            temp = temp->next;
-        }
-        printf("NULL\n");
+        printf("we don't found it \n");
     }
+    else
+    {
+        printf("Found %s \n",tmp ->name);
+    }
+
+
+
+    return 0 ;
+
 }
-
-int main()
-{
-    //init array of list to NULL
-    init();
-
-    insert(7);
-    insert(0);
-    insert(3);
-    insert(10);
-    insert(4);
-    insert(5);
-
-    print();
-
-    return 0;
-}
-
